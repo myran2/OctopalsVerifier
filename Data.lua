5c1525419b58b293fbeea34d5d367c1194871a5e
Henry Gordon <henrytgordon@gmail.com>
2025-03-02 12:01:19 -0500

    remove unused types

--- Data.lua
+++ Data.lua
@@ -98,87 +98,11 @@ function Data:InitDB()
   )
 end
 
----Get stored character by GUID
----@param GUID WOWGUID?
----@return WK_Character|nil
-function Data:GetCharacter(GUID)
-  if GUID == nil then
-    GUID = UnitGUID("player")
-  end
-
-  if GUID == nil then
-    return nil
-  end
-
-  if self.db.global.characters[GUID] == nil then
-    self.db.global.characters[GUID] = Utils:TableCopy(self.defaultCharacter)
-  end
-
-  self.db.global.characters[GUID].GUID = GUID
-
-  return self.db.global.characters[GUID]
-end
-
-function Data:ScanCharacter()
-  local character = self:GetCharacter()
-  if not character then return end
-
-  -- Update character info
-  local localizedRaceName, englishRaceName, raceID = UnitRace("player")
-  local localizedClassName, classFile, classID = UnitClass("player")
-  local englishFactionName, localizedFactionName = UnitFactionGroup("player")
-  character.name = UnitName("player")
-  character.realmName = GetRealmName()
-  character.level = UnitLevel("player")
-  character.factionEnglish = englishFactionName
-  character.factionName = localizedFactionName
-  character.raceID = raceID
-  character.raceEnglish = englishRaceName
-  character.raceName = localizedRaceName
-  character.classID = classID
-  character.classFile = classFile
-  character.className = localizedClassName
-  character.lastUpdate = GetServerTime()
-
-end
-
----Get characters
----@param unfiltered boolean?
----@return WK_Character[]
-function Data:GetCharacters(unfiltered)
-  local characters = Utils:TableFilter(self.db.global.characters, function(character)
-    local include = true
-
-    -- Ignore ghost characters (Bug: https://github.com/DennisRas/OctopalsVerifier/issues/47)
-    if not character.name or character.name == "" then
-      include = false
-    end
-
-    if not unfiltered then
-      if not character.enabled then
-        include = false
-      end
-    end
-
-    return include
-  end)
-
-  table.sort(characters, function(a, b)
-    if type(a.lastUpdate) == "number" and type(b.lastUpdate) == "number" then
-      return a.lastUpdate > b.lastUpdate
-    end
-    return strcmputf8i(a.name, b.name) < 0
-  end)
-
-  return characters
-end
-
 ---Analyze all objectives and their progress
 ---@return Octo_RaidMember[]
 function Data:GetCharactersInRaid()
   ---@type Octo_RaidMember[]
-  local raiders = {
-  }
+  local raiders = {}
   for unit in Utils:IterateGroupMembers() do
     local playerName = GetUnitName(unit, true)
     local classID = select(3, UnitClass("player"))
